f2f2454469628c2a2414e94754d1a42a0538ba79
LM\n0200797 <Abdoulaye.Camara@libertymutual.com>
2017-08-11 18:06:24 -0400

    Removed Folder from repo

--- Java/VendingMachineF/src/main/java/com/mycompany/vendingmachine/service/InvalidItemSelectionException.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.vendingmachine.service;
-
-public class InvalidItemSelectionException extends Exception {
-
-    public String getMessage(){
-        return ("YOU ENTERED AN INVALID ITEM SELECTION!!!!\n"
-                +"PLEASE SELECT FROM THE LIST OF AVAILABLE ITEMS.");
-    }
-}
